d8dc5c6182a00ffe6e903348c26cbafa1ecf77fe
ArachisH <TheOldNutMan@gmail.com>
2014-10-27 15:00:14 -0700

    Will contain the resources the HSession instance needs. ( Should probably use Tuple for this. )

---
+++ Kendax/Services/BotSession.cs
@@ -0,0 +1,17 @@
+﻿using Sulakore.Habbo;
+using System.Windows.Forms;
+using Sulakore.Protocol.Encryption;
+
+namespace Kendax.Services
+{
+    public class BotSession
+    {
+        public HKeyExchange Exchange { get; set; }
+        public ListViewItem SessionItem { get; private set; }
+
+        public BotSession(ListViewItem sessionItem)
+        {
+            SessionItem = sessionItem;
+        }
+    }
+}
